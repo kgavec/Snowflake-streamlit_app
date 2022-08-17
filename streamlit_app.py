76c76cd471a523870809753beabaab5d6c808713
Kevin Gave <38436389+kgavec@users.noreply.github.com>
2022-08-17 16:51:23 -0500

    Update streamlit_app.py
    added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,6 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-
+##another header
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
