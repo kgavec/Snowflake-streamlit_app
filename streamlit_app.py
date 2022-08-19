354ec1226830672aab71407c87e006121ff74607
Kevin Gave <38436389+kgavec@users.noreply.github.com>
2022-08-19 15:58:41 -0500

    Update streamlit_app.py
    Move the Fruit Load List Query and Load into a Button Action

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,33 +40,37 @@ def get_fruityvice_data(this_fruit_choice):
 
 ##New section for API response
 streamlit.header("Fruityvice Fruit Advice!")
+
 try:
-  ##select fruit
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-  else:
-    back_from_function = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
-    
+     ##select fruit
+     fruit_choice = streamlit.text_input('What fruit would you like information about?')
+     if not fruit_choice:
+          streamlit.error("Please select a fruit to get information.")
+     else:
+          back_from_function = get_fruityvice_data(fruit_choice)
+          streamlit.dataframe(back_from_function)
+
 except URLError as e:
-  streamlit.error()
+     streamlit.error()
+
+
+streamlit.header("The fruit load list contains:")
+#snowflake related functions
+def get_fruit_load_list():
+     with my_cnx.cursor() as my_cur:
+          my_cur.execute("SELECT * from fruit_load_list")
+          return my_cur.fetchall()
+
+# Add button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+     ##setup snowflake connection
+     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])   
+     my_data_rows = get_fruit_load_list()
+     streamlit.dataframe(my_data_rows)
 
 #command that stop running anithing past here
 streamlit.stop()
-
-
-##setup snowflake connection
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
-
+     
 ##text input a fruit to add
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
